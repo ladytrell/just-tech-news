c636266e7aed33b299bc893c3480fe48a2c4cfb8
Lady Trell <antrellkent@gmail.com>
2022-05-02 18:43:46 -0400

    Post Model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -62,7 +62,7 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
-    
+
     // Verify user
     const validPassword = dbUserData.checkPassword(req.body.password);
 
