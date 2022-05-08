ff527c73f42e4ae8c049b2337e6e34567fb6021e
Lady Trell <antrellkent@gmail.com>
2022-05-07 20:53:01 -0400

    Add Post Routes

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,32 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+  // Execute as if it were a built in method for Sequelize
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [
+            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count'
+          ]
+        ]
+      });
+    });
+  }
+}
+
 
 Post.init(
     {
@@ -37,6 +62,6 @@ Post.init(
       underscored: true,
       modelName: 'post'
     }
-  );
+);
   
 module.exports = Post;
